d93edd5dc4f49640250374ab49a20fd2059555cf
adrgs <albastroiudragos@gmail.com>
2020-03-31 20:57:47 +0300

    Added python script

--- Teme/TemaVerilog/Problema1/Problema1.py
+++ Teme/TemaVerilog/Problema1/Problema1.py
@@ -5,5 +5,6 @@ for A in [0, 1]:
         for C in [0, 1]:
             outA = int( ((A and B) or (not C)) ^ ((not B) or C) )
             outB = int( ((A or B or C) and (A^C)) or (not B) )
+            outCustom = int( (((A^B) and C) or (A and (B or C))) ^ (not B) )
 
-            print("Time=%d A=%d B=%d C=%d outA=%d outB=%d" % (A*4+B*2+C, A, B, C, outA, outB))
+            print("Time=%d A=%d B=%d C=%d outA=%d outB=%d outCustom=%d" % (A*4+B*2+C, A, B, C, outA, outB, outCustom))
